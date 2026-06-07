741a590a9de6bf72094bfafada2a6200c4313bfa
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-07 16:10:47 +0300

    Delete app/src/main/java/com/winlator/WineService.kt

--- app/src/main/java/com/winlator/WineService.kt
+++
@@ -1,9 +0,0 @@
-package com.winlator
-
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-
-class WineService : Service() {
-    override fun onBind(intent: Intent?): IBinder? = null
-}
