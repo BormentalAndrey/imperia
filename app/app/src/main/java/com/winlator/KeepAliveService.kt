880f724cd7f27634af9ec48355cc73262e765f9a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-06-09 04:03:56 +0300

    Update KeepAliveService.kt

--- app/app/src/main/java/com/winlator/KeepAliveService.kt
+++ app/app/src/main/java/com/winlator/KeepAliveService.kt
@@ -31,12 +31,8 @@ class KeepAliveService : Service() {
             .setOngoing(true)
             .build()
         
-        // Для Android 14+ нужно указывать тип сервиса
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
-        } else {
-            startForeground(NOTIFICATION_ID, notification)
-        }
+        // Простой запуск foreground service (работает на всех версиях)
+        startForeground(NOTIFICATION_ID, notification)
         return START_NOT_STICKY
     }
     
